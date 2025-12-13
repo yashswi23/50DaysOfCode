6d7b211d21ecf79170f64facb6e35dd0babb33fb
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2025-12-13 18:33:35 +0530

    Time: 2 ms (99%), Space: 44.4 MB (67.68%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n = s.length();
+        int[] freq = new int[26];
+
+        for(char ch: s.toCharArray()){
+            freq[ch-'a']++;
+        }
+        for(char ch : t.toCharArray()){
+            freq[ch-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
