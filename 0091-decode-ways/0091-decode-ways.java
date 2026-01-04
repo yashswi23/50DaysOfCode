f6739dcd82a5f28aba376237c27856e1b2ee014e
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2026-01-04 14:22:38 +0530

    Time: 1 ms (85.61%), Space: 43.3 MB (25.27%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1] = (s.charAt(0)=='0')?0:1;
+
+        for(int i=2;i<=n;i++){
+            int one = s.charAt(i-1)-'0';
+            if(one>=1 && one<=9){
+                dp[i]+= dp[i-1];
+            }
+
+            int two = Integer.parseInt(s.substring(i-2,i));
+            if(two>=10 && two<=26){
+                dp[i]+= dp[i-2];
+            }
+        }
+
+        return dp[n];
+
+    }
+}
