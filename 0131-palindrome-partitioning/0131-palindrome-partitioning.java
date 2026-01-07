56a729fc7ae81d85f9d46cb6fbd418a9951dced3
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2026-01-08 00:19:10 +0530

    Time: 7 ms (98.42%), Space: 65.2 MB (76.42%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,35 +1,34 @@
 class Solution {
-    boolean[][]dp;
-
-    public void solve(String s, int i, List<String> curr, List<List<String>> res){
-        if(i==s.length()){
-            res.add(new ArrayList<>(curr));
+     boolean[][]dp ;
+    public void solve(List<List<String>> res, List<String> ans, int i,String s){
+        if(i == s.length()){
+            res.add(new ArrayList<>(ans));
             return;
         }
 
         for(int j=i;j<s.length();j++){
             if(dp[i][j]){
-                curr.add(s.substring(i,j+1));
-                solve(s,j+1,curr,res);
-                curr.remove(curr.size()-1);
+                ans.add(s.substring(i,j+1));
+                solve(res,ans,j+1,s);
+                ans.remove(ans.size()-1);
             }
         }
     }
     public List<List<String>> partition(String s) {
         int n = s.length();
         dp = new boolean[n][n];
-
         for(int i=0;i<n;i++){
-            dp[i][i]=true;
+            dp[i][i] = true;
         }
+
 
         for(int len=2;len<=n;len++){
             for(int i=0;i+len<=n;i++){
-                int j=i+len-1;
+                int j = i+len-1;
 
                 if(s.charAt(i) == s.charAt(j)){
                     if(len==2){
-                        dp[i][j]=true;
+                        dp[i][j] = true;
                     }
                     else{
                         dp[i][j] = dp[i+1][j-1];
@@ -39,8 +38,8 @@ class Solution {
         }
 
         List<List<String>> res = new ArrayList<>();
-        List<String> curr = new ArrayList<>();
-        solve(s,0,curr,res);
+        List<String> ans = new ArrayList<>();
+        solve(res,ans,0,s);
         return res;
     }
 }
