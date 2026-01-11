f456e12699223ae48999c0794f91a603c91dd43a
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2026-01-11 18:03:15 +0530

    Time: 12 ms (43.61%), Space: 46.7 MB (34.92%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,44 +1,47 @@
 class Solution {
     public int calculate(String s) {
-    Stack<Integer> st = new Stack<>();
+        int n = s.length();
+        Stack<Integer> st = new Stack<>();
 
-    int num=0,res=0,sign=1;
+        int res=0;
+        int num=0;
+        int sign=1;
 
-    for(char ch: s.toCharArray()){
-        if(Character.isDigit(ch)){
-            num = num*10+(ch-'0');
+        for(char ch: s.toCharArray()){
+            if(Character.isDigit(ch)){
+                num = num*10+(ch-'0');
+            }
+            else if(ch =='+'){
+                res+=(num*sign);
+                num=0;
+                sign=1;
+            }
+            else if(ch=='-'){
+                res+=(num*sign);
+                num=0;
+                sign=-1;
+            }
+            else if(ch=='('){
+                st.add(res);
+                st.add(sign);
+
+                num=0;
+                res=0;
+                sign=1;
+            }
+            else if(ch==')'){
+                res+=(num*sign);
+                num=0;
+
+                int st_sign = st.pop();
+                int st_res = st.pop();
+                res*=st_sign;
+                res+=st_res;
+            }
 
         }
-        else if(ch=='+'){
-            res+= (num*sign);
-            num=0;
-            sign=1;
-        }
-         else if(ch=='-'){
-            res+= (num*sign);
-            num=0;
-            sign=-1;
-        }
-        else if(ch=='('){
-            st.add(res);
-            st.add(sign);
 
-            num=0;
-            res=0;
-            sign=1;
-        }
-        else if(ch==')'){
-            res+=(num*sign);
-            num=0;
-
-            int st_sign = st.pop();
-            int st_res = st.pop();
-            res*= st_sign;
-            res+=st_res;
-        }
-    }
-    res+=(num*sign);
-    return res;
-
+        res+=(num*sign);
+        return res;
     }
 }
