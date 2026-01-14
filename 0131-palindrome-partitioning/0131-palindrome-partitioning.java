189a2c4cf3215a269e1bf4d646527f58ceb9420d
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2026-01-14 23:10:54 +0530

    Time: 7 ms (98.38%), Space: 65.5 MB (17.88%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,45 +1,46 @@
 class Solution {
-     boolean[][]dp ;
-    public void solve(List<List<String>> res, List<String> ans, int i,String s){
-        if(i == s.length()){
-            res.add(new ArrayList<>(ans));
+    boolean [][] dp ;
+    List<List<String>> res;
+    public void solve(String s, int idx, List<String> curr){
+        if(idx == s.length()){
+            res.add(new ArrayList<>(curr));
             return;
         }
 
-        for(int j=i;j<s.length();j++){
-            if(dp[i][j]){
-                ans.add(s.substring(i,j+1));
-                solve(res,ans,j+1,s);
-                ans.remove(ans.size()-1);
+        for(int j=idx;j<s.length();j++){
+            if(dp[idx][j]){
+            curr.add(s.substring(idx,j+1));
+            solve(s,j+1,curr);
+            curr.remove(curr.size()-1);
             }
         }
     }
+
     public List<List<String>> partition(String s) {
-        int n = s.length();
-        dp = new boolean[n][n];
-        for(int i=0;i<n;i++){
-            dp[i][i] = true;
-        }
+    int n = s.length();
+    dp = new boolean[n][n];
+    for(int i=0;i<n;i++){
+        dp[i][i]= true;
+    }
 
 
-        for(int len=2;len<=n;len++){
-            for(int i=0;i+len<=n;i++){
-                int j = i+len-1;
+   for(int len=2;len<=n;len++){
+    for(int i=0;i+len<=n;i++){
+        int j = i+len-1;
 
-                if(s.charAt(i) == s.charAt(j)){
-                    if(len==2){
-                        dp[i][j] = true;
-                    }
-                    else{
-                        dp[i][j] = dp[i+1][j-1];
-                    }
-                }
+        if(s.charAt(i) == s.charAt(j)){
+            if(len==2){
+                dp[i][j] = true;
+            }
+            else{
+                dp[i][j] = dp[i+1][j-1];
             }
         }
 
-        List<List<String>> res = new ArrayList<>();
-        List<String> ans = new ArrayList<>();
-        solve(res,ans,0,s);
-        return res;
+    }
+   }
+   res = new ArrayList<>();
+   solve(s,0,new ArrayList<>());
+   return res;  
     }
 }
