ff7d12a6fc3af171a24249ddf7d97c231ed4c53e
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2026-01-15 11:38:57 +0530

    Time: 0 ms (100%), Space: 47.6 MB (41.03%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -15,28 +15,22 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-    if(root == null) return null;
-    if(root.val< key){
-        root.right = deleteNode(root.right,key);
-    }
-    else if(root.val> key){
-        root.left = deleteNode(root.left,key);
-    }
-    else{
-        if(root.left == null && root.right == null){
-            return null;
+        if(root == null) return null;
+        if(root.val < key){
+            root.right = deleteNode(root.right,key);
         }
-        else if(root.left == null){
-            return root.right;
+        else if(root.val> key ){
+            root.left = deleteNode(root.left,key);
         }
-        else if(root.right == null){
-            return root.left;
+        else{
+            if(root.left == null && root.right == null) return null;
+            else if(root.left == null) return root.right;
+            else if(root.right == null) return root.left;
+            TreeNode node = findmax(root.left);
+            root.val = node.val;
+            root.left = deleteNode(root.left,node.val);
         }
-        TreeNode node = findmax(root.left);
-        root.val = node.val;
-        root.left = deleteNode(root.left, node.val);
-    }
-    return root;
+        return root;
     }
 
     public TreeNode findmax(TreeNode root){
