0edacac567194e1234ac1994cdc7881dacb9de16
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2026-01-15 11:57:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+       int n = nums.length;
+       int[]dp = new int[n];
+       dp[0] = nums[0];
+       int maxi=nums[0];
+
+       for(int i=1;i<=n;i++){
+        dp[i] = Math.max(dp[i], nums[i]+dp[i-1]);
+        maxi = Math.max(maxi,dp[i]);
+       } 
+
+       return maxi;
+    }
+}
