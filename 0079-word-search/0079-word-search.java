90c0df70c209eb3e5e1d4225323b899fa53b8e9d
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2026-01-29 11:23:19 +0530

    Time: 130 ms (77.73%), Space: 42.9 MB (84.14%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,33 +2,30 @@ class Solution {
     int n;
     int m;
 
-    public boolean isDFS(char[][] board, String word, int i, int j, int idx){
-        if(i<0 || i>=n || j<0 || j>=m || word.charAt(idx)!= board[i][j]) return false;
+    public boolean isFind(int i, int j, char[][] board,String word,int idx){
+        if(i<0 || i>=n || j<0 || j>=m || board[i][j]!= word.charAt(idx)) return false;
 
         if(idx == word.length()-1) return true;
         char temp = board[i][j];
         board[i][j]='#';
-        boolean found =isDFS(board,word,i+1,j,idx+1) ||
-        isDFS(board,word,i-1,j,idx+1) ||
-        isDFS(board,word,i,j+1,idx+1)||
-        isDFS(board,word,i,j-1,idx+1);
+        boolean found = isFind(i+1,j,board,word,idx+1) || isFind(i-1,j,board,word,idx+1) || isFind(i,j+1,board,word,idx+1) || isFind(i,j-1,board,word,idx+1);
         board[i][j] = temp;
 
         return found;
+
+
     }
     public boolean exist(char[][] board, String word) {
         n = board.length;
         m = board[0].length;
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++)
-            {
-                if(word.charAt(0) == board[i][j] && isDFS(board,word,i,j,0)){
+            for(int j=0;j<m;j++){
+                if(word.charAt(0) == board[i][j] && isFind(i,j,board,word,0)){
                     return true;
                 }
             }
         }
-
         return false;
         
     }
