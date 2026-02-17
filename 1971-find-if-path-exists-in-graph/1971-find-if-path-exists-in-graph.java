712ac6259c7f904aacf766df835f7a5a288e2726
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2026-02-17 17:23:41 +0530

    Time: 13 ms (97.73%), Space: 238.3 MB (99.68%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,35 +1,46 @@
 class Solution {
+    int[] parent;
+    int[] rank;
+    public int find(int x){
+        if(x == parent[x]){
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    public void union(int x, int y){
+        int finda = find(x);
+        int findb = find(y);
+
+        if(finda == findb) return;
+
+        if(rank[finda] > rank[findb]){
+            parent[findb] = finda;
+        }
+        else if(rank[findb] > rank[finda]){
+            parent[finda] = findb;
+        }
+        else{
+            parent[findb] = finda;
+            rank[finda]++;
+        }
+    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> adj  = new ArrayList<>();
-
+        parent = new int[n];
+        rank = new int[n];
         for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
+        parent[i] = i;
+        rank[i]=0;
+       }
 
-        for(int[]e: edges){
-            int u = e[0];
-            int v = e[1];
+       for(int []e: edges){
+        int u = e[0];
+        int v = e[1];
 
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-        int[]vis = new int[n];
-        Arrays.fill(vis,-1);
-        Queue<Integer> q = new LinkedList<>();
-        q.add(source);
-        vis[source]=1;
-        while(!q.isEmpty()){
-            int node = q.poll();
-            if(node == destination)return true;
+        union(u,v);
+       }
 
-            for(Integer it: adj.get(node)){
-                if(vis[it]==-1){
-                    q.add(it);
-                    vis[it] = 1;
-                }
-            }
-        }
-
-        return false;
+       return find(source) == find(destination);
     }
 }
